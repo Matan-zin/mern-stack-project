1e43fd5869a03dc62bf387189c34c4401703202d
Matan zin <zinmatan@protonmail.com>
2021-12-12 17:30:39 +0200

    improve welcome page apperance

--- react_client/src/pages/welcome.js
+++ react_client/src/pages/welcome.js
@@ -1,5 +1,8 @@
+import projector from '../assets/Projector.svg'
+import '../styles/welcome.css';
 export default function Welcome({ username }) {
-    return <div className="jumbo">
+    return <div className="welcome-page-wrraper">
            <h1>Welcome { username } </h1>
+           <img src={projector} alt="penguin image" />
            </div>
 }
